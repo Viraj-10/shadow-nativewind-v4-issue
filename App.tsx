a9a164414c271e4b8a513eb0d06f6a06027e4554
viraj-10 <virajjoshi132@gmail.com>
2024-10-08 19:22:15 +0530

    issue reproduction

--- App.tsx
+++ App.tsx
@@ -1,13 +1,33 @@
-import { ScreenContent } from 'components/ScreenContent';
-import { StatusBar } from 'expo-status-bar';
-
 import './global.css';
+import { Text, View } from 'react-native';
+import { vars } from 'nativewind';
 
+const config = {
+  light: vars({ '--brand-color': 'red' }),
+  dark: vars({ '--brand-color': 'blue' }),
+};
 export default function App() {
   return (
-    <>
-      <ScreenContent title="Home" path="App.tsx" />
-      <StatusBar style="auto" />
-    </>
+    <View
+      style={[
+        config['light'],
+        {
+          flex: 1,
+          height: '100%',
+          width: '100%',
+          justifyContent: 'center',
+          alignItems: 'center',
+          gap: 10,
+        },
+      ]}>
+      <View className="shadow">
+        <View className="h-7 w-7 border-2 border-red-500 bg-[--brand-color]"></View>
+      </View>
+      <Text>with Shadow</Text>
+      <View>
+        <View className="h-7 w-7 border-2 border-red-500 bg-[--brand-color]" />
+      </View>
+      <Text>without Shadow</Text>
+    </View>
   );
 }
